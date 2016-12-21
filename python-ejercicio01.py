0db36b3213914c19a02710e62573ee12e4e26e1b
Pedro Terrón <pedrotd92@gmail.com>
2016-12-21 20:24:04 +0100

    Cambios1

--- python-ejercicio01.py
+++ python-ejercicio01.py
@@ -1,4 +1,4 @@
-# ejercicios-012901440291-ipppd.py
+# ejercicios-012901440291-ipppd.py sñlkvnñvld´ln
 # Introducción a la Programación con Python y los Paradigmas de Datos
 
 # Práctica 1: Introducción a Python
